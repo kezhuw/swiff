a7ac145ca053f1a07703d3b3785dffedd1374f44
Kezhu Wang <kezhuw@gmail.com>
2011-06-21 17:39:15 +0800

    Add multi-platform header file: base/multip.h.

---
+++ base/multip.h
@@ -0,0 +1,13 @@
+#ifndef __MULTIP_H
+#define __MULTIP_H
+
+// Platform dependent header file.
+
+// Support only full-os currently.
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <setjmp.h>
+#include <assert.h>
+
+#endif
