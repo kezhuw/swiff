c8c01eca42d5ba6d24b477252e191f50454a66a7
Kezhu Wang <kezhuw@gmail.com>
2012-03-19 02:14:29 +0800

    Remove assert module.

--- base/assert.c
+++
@@ -1,9 +0,0 @@
-#include "assert.h"
-#include "except.h"
-
-const Exception_t ExceptionAssert = except_define("Assertion failure");
-
-void
-assert_throw(const char *spec, const char *func, const char *file, int line) {
-	(except_throw)(ExceptionAssert, spec, func, file, line);
-}
